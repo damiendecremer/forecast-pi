f23e886511dfd5cf659da8fc4b7610d0279324a0
sieste <stefan_siegert@gmx.de>
2013-12-26 00:24:09 +0100

    some comments in publish routine

--- publish-txt.sh
+++ publish-txt.sh
@@ -1,8 +1,10 @@
+# get MOSTRECENT figure file
 file=`grep ^fig.*txt$ MOSTRECENT`
 cp -f $file exeter-precip/exeter-precip.txt
+
+# commit and push change to github
 cd exeter-precip
 git add .
 git commit -m "latest commit"
 git push origin master
-
 
