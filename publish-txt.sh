1457df3a9287000eb382ca912a5b4d7d230c6e6e
sieste <stefan_siegert@gmx.de>
2013-12-26 00:46:40 +0100

    specified publish routine

--- publish-txt.sh
+++ publish-txt.sh
@@ -1,10 +1,10 @@
 # get MOSTRECENT figure file
-file=`grep ^fig.*txt$ MOSTRECENT`
-cp -f $file exeter-precip/exeter-precip.txt
+file=`grep ^fig.*precip-onehour.txt$ MOSTRECENT`
+cp -f $file pub/exeter-precip-onehour.txt
 
 # commit and push change to github
-cd exeter-precip
-git add .
-git commit -m "latest commit"
+git add pub/exeter-precip-onehour.txt
+git commit -m "precip one hour `date +%Y%m%d-%H%M`"
 git push origin master
+
 
