210afc48739e82366dc01cef453f165274ac356a
sieste <stefan_siegert@gmx.de>
2013-12-26 01:21:07 +0100

    removed forecast.io id

--- get-forecastio-data.sh
+++ get-forecastio-data.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+forecast_id = `cat my-forecast-io-id.txt`
+
 #place where data is stored
 datadir=data
 mkdir -p $datadir
@@ -12,7 +14,7 @@ jsonfile=$datadir/exeter-$now.json
 if [ ! -e jsonfile ]
 then
   # get exeter data from forecast.io and write to jsonfile
-  wget "https://api.forecast.io/forecast/a5508a7280695c4b560b58f19a7c702c/50.7166,-3.5333" -O $jsonfile
+  wget "https://api.forecast.io/forecast/$forecast_id/50.7166,-3.5333" -O $jsonfile
 fi
 
 # update mostrecent file
